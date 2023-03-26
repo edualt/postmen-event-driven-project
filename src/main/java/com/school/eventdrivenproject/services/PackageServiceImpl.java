e43f6b4678523ee0c31fad9c2e06eea405a4348a
edualt <eduardosebastianhdz503@gmail.com>
2023-03-26 13:42:41 -0600

    feat(services): add package service implementation

---
+++ src/main/java/com/school/eventdrivenproject/services/PackageServiceImpl.java
@@ -0,0 +1,29 @@
+package com.school.eventdrivenproject.services;
+
+import com.school.eventdrivenproject.dtos.requests.CreatePackageRequest;
+import com.school.eventdrivenproject.entities.Package;
+import com.school.eventdrivenproject.repositories.IPackageRepository;
+import com.school.eventdrivenproject.services.interfaces.IPackageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PackageServiceImpl implements IPackageService {
+
+    @Autowired
+    private IPackageRepository repository;
+
+    @Override
+    public Package create(CreatePackageRequest request) {
+        Package packageResponse = from(request);
+        return repository.save(packageResponse);
+    }
+
+    private Package from(CreatePackageRequest request){
+        Package packageResponse = new Package();
+        packageResponse.setHeight(request.getHeight());
+        packageResponse.setWidth(request.getWidth());
+        packageResponse.setWeight(request.getWeight());
+        return packageResponse;
+    }
+}

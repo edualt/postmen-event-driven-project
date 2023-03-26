0e15661ff82bad13ef4a436a9ec027441ecdcddf
edualt <eduardosebastianhdz503@gmail.com>
2023-03-26 03:19:32 -0600

    feat(services): change methods name and parameters

--- src/main/java/com/school/eventdrivenproject/services/interfaces/IOrderService.java
+++ src/main/java/com/school/eventdrivenproject/services/interfaces/IOrderService.java
@@ -1,17 +1,22 @@
 package com.school.eventdrivenproject.services.interfaces;
 
-import com.school.eventdrivenproject.controllers.dtos.requests.CreateOrderRequest;
-import com.school.eventdrivenproject.controllers.dtos.responses.BaseResponse;
+import com.school.eventdrivenproject.dtos.requests.CreateOrderRequest;
+import com.school.eventdrivenproject.dtos.requests.CreatePackageRequest;
+import com.school.eventdrivenproject.dtos.responses.BaseResponse;
 import com.school.eventdrivenproject.entities.Order;
 
+import java.io.IOException;
+
 public interface IOrderService {
-    Order create(CreateOrderRequest request);
+    Order create(CreateOrderRequest orderRequest, CreatePackageRequest packageRequest) throws IOException;
 
     BaseResponse list();
 
     Order get(Long id);
 
-    BaseResponse update();
+    Order updateStatusToInProgress(String trackingId);
+
+    Order updateStatusToDelivered(String trackingId);
 
     BaseResponse delete(Long id);
 

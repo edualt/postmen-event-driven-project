16657c94d3cc82581cade6395b1f93dfed294fb3
edualt <eduardosebastianhdz503@gmail.com>
2023-03-26 13:34:46 -0600

    feat(repositories): add new methods to find

--- src/main/java/com/school/eventdrivenproject/repositories/IOrderStatusRepository.java
+++ src/main/java/com/school/eventdrivenproject/repositories/IOrderStatusRepository.java
@@ -4,9 +4,13 @@ import com.school.eventdrivenproject.entities.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IOrderStatusRepository extends JpaRepository<OrderStatus, Long> {
 
     OrderStatus findOneByName(String name);
+
+    Optional<OrderStatus> findOrderStatusByName(String name);
 
 }

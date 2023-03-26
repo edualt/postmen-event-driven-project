e1769d4dabadad386a4ac1197426c3ca1242898e
edualt <eduardosebastianhdz503@gmail.com>
2023-03-26 03:16:41 -0600

    feat(services): add event service implementation

--- src/main/java/com/school/eventdrivenproject/services/EventServiceImpl.java
+++ src/main/java/com/school/eventdrivenproject/services/EventServiceImpl.java
@@ -1,7 +1,6 @@
 package com.school.eventdrivenproject.services;
 
-import com.school.eventdrivenproject.controllers.dtos.requests.CreateEventRequest;
-import com.school.eventdrivenproject.controllers.dtos.responses.BaseResponse;
+import com.school.eventdrivenproject.dtos.requests.CreateEventRequest;
 import com.school.eventdrivenproject.entities.Event;
 import com.school.eventdrivenproject.entities.Order;
 import com.school.eventdrivenproject.repositories.IEventRepository;
@@ -9,8 +8,10 @@ import com.school.eventdrivenproject.services.interfaces.IEventService;
 import com.school.eventdrivenproject.services.interfaces.IOrderService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Date;
 
 @Service
 public class EventServiceImpl implements IEventService {
@@ -21,14 +22,15 @@ public class EventServiceImpl implements IEventService {
     @Autowired
     private IOrderService orderService;
 
-
     @Override
     @Transactional
-    public Event create(CreateEventRequest request) {
-        Order order = orderService.create(request.getOrder());
+    public Event create(CreateEventRequest request) throws IOException {
+        Order order = orderService.create(request.getOrder(), request.getOrder().getPackageRequest());
+
         Event event = new Event();
         event.setType(request.getType());
         event.setOrder(order);
+        event.setDate(new Date());
         return repository.save(event);
     }
 
